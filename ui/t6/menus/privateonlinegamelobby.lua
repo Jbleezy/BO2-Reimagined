0f45f265fea015079f806d57c9dfeb45bf8a7fc2
Jbleezy <thejbirdjustin@yahoo.com>
2026-01-05 03:53:57 -0800

    Change solo play lobby title

--- ui/t6/menus/privateonlinegamelobby.lua
+++ ui/t6/menus/privateonlinegamelobby.lua
@@ -8,6 +8,11 @@ LUI.createMenu.PrivateOnlineGameLobby = function(f1_arg0)
 	end
 
 	local f1_local1 = Engine.Localize("MPUI_CUSTOM_GAMES_CAPS")
+
+	if CoD.isZombie and UIExpression.DvarBool(nil, "party_solo") == 1 then
+		f1_local1 = Engine.Localize("ZMUI_SOLO_PLAY_CAPS")
+	end
+
 	f1_local0:addTitle(f1_local1)
 	f1_local0.panelManager.panels.buttonPane.titleText = f1_local1
 
