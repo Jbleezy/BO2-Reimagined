754ba2fbc8045bcc508971e1f45452ff3105e3c3
Jbleezy <thejbirdjustin@yahoo.com>
2026-01-05 03:50:25 -0800

    Move ui_gametype_obj_lobby and ui_gametype_pro_lobby dvars to privateonlinegamelobby.lua

--- ui/t6/menus/privateonlinegamelobby.lua
+++ ui/t6/menus/privateonlinegamelobby.lua
@@ -2,12 +2,24 @@ require("T6.Menus.PrivateGameLobby")
 CoD.PrivateOnlineGameLobby = {}
 LUI.createMenu.PrivateOnlineGameLobby = function(f1_arg0)
 	local f1_local0 = CoD.PrivateGameLobby.New("PrivateOnlineGameLobby", f1_arg0)
+
 	if CoD.isMultiplayer then
 		f1_local0:setPreviousMenu("MainLobby")
 	end
+
 	local f1_local1 = Engine.Localize("MPUI_CUSTOM_GAMES_CAPS")
 	f1_local0:addTitle(f1_local1)
 	f1_local0.panelManager.panels.buttonPane.titleText = f1_local1
+
+	if UIExpression.DvarString(nil, "ui_gametype_obj_lobby") ~= "" then
+		Engine.SetDvar("ui_gametype_obj", UIExpression.DvarString(nil, "ui_gametype_obj_lobby"))
+		Engine.SetDvar("ui_gametype_obj_lobby", "")
+	end
+
+	if UIExpression.DvarString(nil, "ui_gametype_pro_lobby") ~= "" then
+		Engine.SetDvar("ui_gametype_pro", UIExpression.DvarString(nil, "ui_gametype_pro_lobby"))
+		Engine.SetDvar("ui_gametype_pro_lobby", "")
+	end
 
 	if CoD.PrivateGameLobby.FadeIn == true then
 		CoD.PrivateGameLobby.FadeIn = nil
