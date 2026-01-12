7c09a39cd0b0fb8903ee876765fbc596ad545b18
Jbleezy <thejbirdjustin@yahoo.com>
2026-01-12 02:54:15 -0800

    Optimize selectmaplistzombie.lua

--- ui_mp/t6/zombie/selectmaplistzombie.lua
+++ ui_mp/t6/zombie/selectmaplistzombie.lua
@@ -3,191 +3,218 @@ require("T6.Menus.PopupMenus")
 require("T6.ListBox")
 
 CoD.SelectMapListZombie = {}
-CoD.SelectMapListZombie.GameModes = {
-	"ZMUI_ZCLASSIC_GAMEMODE_CAPS",
-	"ZMUI_ZSTANDARD_CAPS",
-	"ZMUI_ZGRIEF_CAPS",
-	"ZMUI_ZRACE_CAPS",
-	"ZMUI_ZCONTAINMENT_CAPS",
-	"ZMUI_ZMEAT_CAPS",
-	"ZMUI_ZSR_CAPS",
-	-- "ZMUI_ZCLEANSED_CAPS", -- TODO: add
+CoD.SelectMapListZombie.GameModes = {}
+CoD.SelectMapListZombie.GameModes[1] = {
+	localizedString = "ZMUI_ZCLASSIC_GAMEMODE_CAPS",
+	ui_zm_gamemodegroup = "zclassic",
+	ui_gametype = "zclassic",
+	ui_gametype_obj = "",
 }
-CoD.SelectMapListZombie.Maps = {
-	"ZMUI_ZCLASSIC_ZM_TRANSIT_CAPS",
-	"ZMUI_ZCLASSIC_ZM_HIGHRISE_CAPS",
-	"ZMUI_ZCLASSIC_ZM_BURIED_CAPS",
-	"ZMUI_ZCLASSIC_ZM_PRISON_CAPS",
-	"ZMUI_ZCLASSIC_ZM_TOMB_CAPS",
+CoD.SelectMapListZombie.GameModes[2] = {
+	localizedString = "ZMUI_ZSTANDARD_CAPS",
+	ui_zm_gamemodegroup = "zsurvival",
+	ui_gametype = "zstandard",
+	ui_gametype_obj = "",
 }
-CoD.SelectMapListZombie.Locations = {
-	"ZMUI_NUKED_STARTLOC_CAPS",
-	"ZMUI_TRANSIT_STARTLOC_CAPS",
-	"ZMUI_DINER_CAPS",
-	"ZMUI_FARM_CAPS",
-	"ZMUI_POWER_CAPS",
-	"ZMUI_TOWN_CAPS",
-	"ZMUI_TUNNEL_CAPS",
-	"ZMUI_CORNFIELD_CAPS",
-	"ZMUI_SHOPPING_MALL_CAPS",
-	"ZMUI_DRAGON_ROOFTOP_CAPS",
-	"ZMUI_SWEATSHOP_CAPS",
-	"ZMUI_STREET_LOC_CAPS",
-	"ZMUI_MAZE_CAPS",
-	"ZMUI_CELLBLOCK_CAPS",
-	"ZMUI_DOCKS_CAPS",
-	"ZMUI_TRENCHES_CAPS",
-	"ZMUI_EXCAVATION_SITE_CAPS",
-	"ZMUI_CHURCH_CAPS",
-	"ZMUI_CRAZY_PLACE_CAPS",
+CoD.SelectMapListZombie.GameModes[3] = {
+	localizedString = "ZMUI_ZGRIEF_CAPS",
+	ui_zm_gamemodegroup = "zencounter",
+	ui_gametype = "zgrief",
+	ui_gametype_obj = "zgrief",
 }
+CoD.SelectMapListZombie.GameModes[4] = {
+	localizedString = "ZMUI_ZRACE_CAPS",
+	ui_zm_gamemodegroup = "zencounter",
+	ui_gametype = "zgrief",
+	ui_gametype_obj = "zrace",
+}
+CoD.SelectMapListZombie.GameModes[5] = {
+	localizedString = "ZMUI_ZCONTAINMENT_CAPS",
+	ui_zm_gamemodegroup = "zencounter",
+	ui_gametype = "zgrief",
+	ui_gametype_obj = "zcontainment",
+}
+CoD.SelectMapListZombie.GameModes[6] = {
+	localizedString = "ZMUI_ZMEAT_CAPS",
+	ui_zm_gamemodegroup = "zencounter",
+	ui_gametype = "zgrief",
+	ui_gametype_obj = "zmeat",
+}
+CoD.SelectMapListZombie.GameModes[7] = {
+	localizedString = "ZMUI_ZSR_CAPS",
+	ui_zm_gamemodegroup = "zencounter",
+	ui_gametype = "zgrief",
+	ui_gametype_obj = "zsr",
+}
+CoD.SelectMapListZombie.Maps = {}
+CoD.SelectMapListZombie.Maps[1] = {
+	localizedString = "ZMUI_ZCLASSIC_ZM_TRANSIT_CAPS",
+	ui_mapname = "zm_transit",
+	ui_zm_mapstartlocation = "transit",
+}
+CoD.SelectMapListZombie.Maps[2] = {
+	localizedString = "ZMUI_ZCLASSIC_ZM_HIGHRISE_CAPS",
+	ui_mapname = "zm_highrise",
+	ui_zm_mapstartlocation = "rooftop",
+}
+CoD.SelectMapListZombie.Maps[3] = {
+	localizedString = "ZMUI_ZCLASSIC_ZM_BURIED_CAPS",
+	ui_mapname = "zm_buried",
+	ui_zm_mapstartlocation = "processing",
+}
+CoD.SelectMapListZombie.Maps[4] = {
+	localizedString = "ZMUI_ZCLASSIC_ZM_PRISON_CAPS",
+	ui_mapname = "zm_prison",
+	ui_zm_mapstartlocation = "prison",
+}
+CoD.SelectMapListZombie.Maps[5] = {
+	localizedString = "ZMUI_ZCLASSIC_ZM_TOMB_CAPS",
+	ui_mapname = "zm_tomb",
+	ui_zm_mapstartlocation = "tomb",
+}
+CoD.SelectMapListZombie.Locations = {}
+CoD.SelectMapListZombie.Locations[1] = {
+	localizedString = "ZMUI_NUKED_STARTLOC_CAPS",
+	ui_mapname = "zm_nuked",
+	ui_zm_mapstartlocation = "nuked",
+}
+CoD.SelectMapListZombie.Locations[2] = {
+	localizedString = "ZMUI_TRANSIT_STARTLOC_CAPS",
+	ui_mapname = "zm_transit",
+	ui_zm_mapstartlocation = "transit",
+}
+CoD.SelectMapListZombie.Locations[3] = {
+	localizedString = "ZMUI_DINER_CAPS",
+	ui_mapname = "zm_transit",
+	ui_zm_mapstartlocation = "diner",
+}
+CoD.SelectMapListZombie.Locations[4] = {
+	localizedString = "ZMUI_FARM_CAPS",
+	ui_mapname = "zm_transit",
+	ui_zm_mapstartlocation = "farm",
+}
+CoD.SelectMapListZombie.Locations[5] = {
+	localizedString = "ZMUI_POWER_CAPS",
+	ui_mapname = "zm_transit",
+	ui_zm_mapstartlocation = "power",
+}
+CoD.SelectMapListZombie.Locations[6] = {
+	localizedString = "ZMUI_TOWN_CAPS",
+	ui_mapname = "zm_transit",
+	ui_zm_mapstartlocation = "town",
+}
+CoD.SelectMapListZombie.Locations[7] = {
+	localizedString = "ZMUI_TUNNEL_CAPS",
+	ui_mapname = "zm_transit",
+	ui_zm_mapstartlocation = "tunnel",
+}
+CoD.SelectMapListZombie.Locations[8] = {
+	localizedString = "ZMUI_CORNFIELD_CAPS",
+	ui_mapname = "zm_transit",
+	ui_zm_mapstartlocation = "cornfield",
+}
+CoD.SelectMapListZombie.Locations[9] = {
+	localizedString = "ZMUI_SHOPPING_MALL_CAPS",
+	ui_mapname = "zm_highrise",
+	ui_zm_mapstartlocation = "shopping_mall",
+}
+CoD.SelectMapListZombie.Locations[10] = {
+	localizedString = "ZMUI_DRAGON_ROOFTOP_CAPS",
+	ui_mapname = "zm_highrise",
+	ui_zm_mapstartlocation = "dragon_rooftop",
+}
+CoD.SelectMapListZombie.Locations[11] = {
+	localizedString = "ZMUI_SWEATSHOP_CAPS",
+	ui_mapname = "zm_highrise",
+	ui_zm_mapstartlocation = "sweatshop",
+}
+CoD.SelectMapListZombie.Locations[12] = {
+	localizedString = "ZMUI_STREET_LOC_CAPS",
+	ui_mapname = "zm_buried",
+	ui_zm_mapstartlocation = "street",
+}
+CoD.SelectMapListZombie.Locations[13] = {
+	localizedString = "ZMUI_MAZE_CAPS",
+	ui_mapname = "zm_buried",
+	ui_zm_mapstartlocation = "maze",
+}
+CoD.SelectMapListZombie.Locations[14] = {
+	localizedString = "ZMUI_CELLBLOCK_CAPS",
+	ui_mapname = "zm_prison",
+	ui_zm_mapstartlocation = "cellblock",
+}
+CoD.SelectMapListZombie.Locations[15] = {
+	localizedString = "ZMUI_DOCKS_CAPS",
+	ui_mapname = "zm_prison",
+	ui_zm_mapstartlocation = "docks",
+}
+CoD.SelectMapListZombie.Locations[16] = {
+	localizedString = "ZMUI_TRENCHES_CAPS",
+	ui_mapname = "zm_tomb",
+	ui_zm_mapstartlocation = "trenches",
+}
+CoD.SelectMapListZombie.Locations[17] = {
+	localizedString = "ZMUI_EXCAVATION_SITE_CAPS",
+	ui_mapname = "zm_tomb",
+	ui_zm_mapstartlocation = "excavation_site",
+}
+CoD.SelectMapListZombie.Locations[18] = {
+	localizedString = "ZMUI_CHURCH_CAPS",
+	ui_mapname = "zm_tomb",
+	ui_zm_mapstartlocation = "church",
+}
+CoD.SelectMapListZombie.Locations[19] = {
+	localizedString = "ZMUI_CRAZY_PLACE_CAPS",
+	ui_mapname = "zm_tomb",
+	ui_zm_mapstartlocation = "crazy_place",
+}
+
+local function getKeyValueIndex(table, key, value)
+	for i, v in ipairs(table) do
+		if v[key] == value then
+			return i
+		end
+	end
+
+	return 0
+end
 
 local function setGameModeDvars()
-	local index = math.max(1, UIExpression.DvarInt(nil, "ui_gametype_index"))
-	local gameMode = CoD.SelectMapListZombie.GameModes[index]
+	local index = CoD.SelectMapListZombie.GameModeIndex
 
-	if gameMode == "ZMUI_ZCLASSIC_GAMEMODE_CAPS" then
-		Engine.SetDvar("ui_zm_gamemodegroup", "zclassic")
-		Engine.SetGametype("zclassic")
-		Engine.SetDvar("ui_gametype_obj", "")
-	elseif gameMode == "ZMUI_ZSTANDARD_CAPS" then
-		Engine.SetDvar("ui_zm_gamemodegroup", "zsurvival")
-		Engine.SetGametype("zstandard")
-		Engine.SetDvar("ui_gametype_obj", "")
-	elseif gameMode == "ZMUI_ZGRIEF_CAPS" then
-		Engine.SetDvar("ui_zm_gamemodegroup", "zencounter")
-		Engine.SetGametype("zgrief")
-		Engine.SetDvar("ui_gametype_obj", "zgrief")
-	elseif gameMode == "ZMUI_ZRACE_CAPS" then
-		Engine.SetDvar("ui_zm_gamemodegroup", "zencounter")
-		Engine.SetGametype("zgrief")
-		Engine.SetDvar("ui_gametype_obj", "zrace")
-	elseif gameMode == "ZMUI_ZCONTAINMENT_CAPS" then
-		Engine.SetDvar("ui_zm_gamemodegroup", "zencounter")
-		Engine.SetGametype("zgrief")
-		Engine.SetDvar("ui_gametype_obj", "zcontainment")
-	elseif gameMode == "ZMUI_ZMEAT_CAPS" then
-		Engine.SetDvar("ui_zm_gamemodegroup", "zencounter")
-		Engine.SetGametype("zgrief")
-		Engine.SetDvar("ui_gametype_obj", "zmeat")
-	elseif gameMode == "ZMUI_ZSR_CAPS" then
-		Engine.SetDvar("ui_zm_gamemodegroup", "zencounter")
-		Engine.SetGametype("zgrief")
-		Engine.SetDvar("ui_gametype_obj", "zsr")
-	elseif gameMode == "ZMUI_ZCLEANSED_CAPS" then
-		-- TODO: set dvars when game mode is added
-	end
+	Engine.SetDvar("ui_zm_gamemodegroup", CoD.SelectMapListZombie.GameModes[index].ui_zm_gamemodegroup)
+	Engine.SetGametype(CoD.SelectMapListZombie.GameModes[index].ui_gametype)
+	Engine.SetDvar("ui_gametype_obj", CoD.SelectMapListZombie.GameModes[index].ui_gametype_obj)
 end
 
 local function setMapDvars()
-	local index = math.max(1, UIExpression.DvarInt(nil, "ui_mapname_index"))
-	local map = CoD.SelectMapListZombie.Maps[index]
+	local index = CoD.SelectMapListZombie.MapIndex
 
-	if map == "ZMUI_ZCLASSIC_ZM_TRANSIT_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_transit")
-		Engine.SetDvar("ui_zm_mapstartlocation", "transit")
-	elseif map == "ZMUI_ZCLASSIC_ZM_HIGHRISE_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_highrise")
-		Engine.SetDvar("ui_zm_mapstartlocation", "rooftop")
-	elseif map == "ZMUI_ZCLASSIC_ZM_BURIED_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_buried")
-		Engine.SetDvar("ui_zm_mapstartlocation", "processing")
-	elseif map == "ZMUI_ZCLASSIC_ZM_PRISON_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_prison")
-		Engine.SetDvar("ui_zm_mapstartlocation", "prison")
-	elseif map == "ZMUI_ZCLASSIC_ZM_TOMB_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_tomb")
-		Engine.SetDvar("ui_zm_mapstartlocation", "tomb")
-	end
+	Engine.SetDvar("ui_mapname", CoD.SelectMapListZombie.Maps[index].ui_mapname)
+	Engine.SetDvar("ui_zm_mapstartlocation", CoD.SelectMapListZombie.Maps[index].ui_zm_mapstartlocation)
 end
 
 local function setLocationDvars()
-	local index = math.max(1, UIExpression.DvarInt(nil, "ui_zm_mapstartlocation_index"))
-	local location = CoD.SelectMapListZombie.Locations[index]
+	local index = CoD.SelectMapListZombie.LocationIndex
 
-	if location == "ZMUI_TRANSIT_STARTLOC_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_transit")
-		Engine.SetDvar("ui_zm_mapstartlocation", "transit")
-	elseif location == "ZMUI_DINER_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_transit")
-		Engine.SetDvar("ui_zm_mapstartlocation", "diner")
-	elseif location == "ZMUI_FARM_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_transit")
-		Engine.SetDvar("ui_zm_mapstartlocation", "farm")
-	elseif location == "ZMUI_POWER_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_transit")
-		Engine.SetDvar("ui_zm_mapstartlocation", "power")
-	elseif location == "ZMUI_TOWN_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_transit")
-		Engine.SetDvar("ui_zm_mapstartlocation", "town")
-	elseif location == "ZMUI_TUNNEL_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_transit")
-		Engine.SetDvar("ui_zm_mapstartlocation", "tunnel")
-	elseif location == "ZMUI_CORNFIELD_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_transit")
-		Engine.SetDvar("ui_zm_mapstartlocation", "cornfield")
-	elseif location == "ZMUI_NUKED_STARTLOC_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_nuked")
-		Engine.SetDvar("ui_zm_mapstartlocation", "nuked")
-	elseif location == "ZMUI_SHOPPING_MALL_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_highrise")
-		Engine.SetDvar("ui_zm_mapstartlocation", "shopping_mall")
-	elseif location == "ZMUI_DRAGON_ROOFTOP_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_highrise")
-		Engine.SetDvar("ui_zm_mapstartlocation", "dragon_rooftop")
-	elseif location == "ZMUI_SWEATSHOP_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_highrise")
-		Engine.SetDvar("ui_zm_mapstartlocation", "sweatshop")
-	elseif location == "ZMUI_STREET_LOC_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_buried")
-		Engine.SetDvar("ui_zm_mapstartlocation", "street")
-	elseif location == "ZMUI_MAZE_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_buried")
-		Engine.SetDvar("ui_zm_mapstartlocation", "maze")
-	elseif location == "ZMUI_CELLBLOCK_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_prison")
-		Engine.SetDvar("ui_zm_mapstartlocation", "cellblock")
-	elseif location == "ZMUI_DOCKS_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_prison")
-		Engine.SetDvar("ui_zm_mapstartlocation", "docks")
-	elseif location == "ZMUI_TRENCHES_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_tomb")
-		Engine.SetDvar("ui_zm_mapstartlocation", "trenches")
-	elseif location == "ZMUI_EXCAVATION_SITE_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_tomb")
-		Engine.SetDvar("ui_zm_mapstartlocation", "excavation_site")
-	elseif location == "ZMUI_CHURCH_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_tomb")
-		Engine.SetDvar("ui_zm_mapstartlocation", "church")
-	elseif location == "ZMUI_CRAZY_PLACE_CAPS" then
-		Engine.SetDvar("ui_mapname", "zm_tomb")
-		Engine.SetDvar("ui_zm_mapstartlocation", "crazy_place")
-	end
+	Engine.SetDvar("ui_mapname", CoD.SelectMapListZombie.Locations[index].ui_mapname)
+	Engine.SetDvar("ui_zm_mapstartlocation", CoD.SelectMapListZombie.Locations[index].ui_zm_mapstartlocation)
 end
 
 local function gameModeListFocusChangedEventHandler(self, event)
-	local focusedIndex = self.listBox:getFocussedIndex()
+	CoD.SelectMapListZombie.GameModeIndex = self.listBox:getFocussedIndex()
 end
 
 local function gameModeListSelectionClickedEventHandler(self, event)
-	local focusedIndex = self.listBox:getFocussedIndex()
+	CoD.SelectMapListZombie.GameModeIndex = self.listBox:getFocussedIndex()
 
-	CoD.SelectMapListZombie.GameModeIndex = focusedIndex
-
-	local gameMode = CoD.SelectMapListZombie.GameModes[focusedIndex]
-
-	if gameMode == "ZMUI_ZCLASSIC_GAMEMODE_CAPS" then
+	if CoD.SelectMapListZombie.GameModes[CoD.SelectMapListZombie.GameModeIndex].ui_gametype == "zclassic" then
 		self:openMenu("SelectMapListZM", self.controller)
 	else
 		self:openMenu("SelectLocationListZM", self.controller)
 	end
 
 	self:close()
-end
-
-local function gameModeListBackEventHandler(self, event)
-	CoD.SelectMapListZombie.GameModeIndex = nil
-	CoD.Menu.ButtonPromptBack(self, event)
 end
 
 local function gameModeListCreateButtonMutables(controller, mutables)
@@ -201,8 +228,7 @@ local function gameModeListCreateButtonMutables(controller, mutables)
 end
 
 local function gameModeListGetButtonData(controller, index, mutables, self)
-	local gameMode = CoD.SelectMapListZombie.GameModes[index]
-	mutables.text:setText(Engine.Localize(gameMode))
+	mutables.text:setText(Engine.Localize(CoD.SelectMapListZombie.GameModes[index].localizedString))
 end
 
 function LUI.createMenu.SelectGameModeListZM(controller)
@@ -217,7 +243,13 @@ function LUI.createMenu.SelectGameModeListZM(controller)
 
 	self:addTitle(Engine.Localize("MPUI_GAMEMODE_CAPS"))
 
-	local index = math.max(1, UIExpression.DvarInt(nil, "ui_gametype_index"))
+	local index = 1
+
+	if UIExpression.DvarString(nil, "ui_gametype") == "zgrief" then
+		index = getKeyValueIndex(CoD.SelectMapListZombie.GameModes, "ui_gametype_obj", UIExpression.DvarString(nil, "ui_gametype_obj"))
+	else
+		index = getKeyValueIndex(CoD.SelectMapListZombie.GameModes, "ui_gametype", UIExpression.DvarString(nil, "ui_gametype"))
+	end
 
 	local listBox = CoD.ListBox.new(nil, controller, 15, CoD.CoD9Button.Height, 250, gameModeListCreateButtonMutables, gameModeListGetButtonData, 5, 0)
 	listBox:setLeftRight(true, false, 0, 250)
@@ -233,7 +265,6 @@ function LUI.createMenu.SelectGameModeListZM(controller)
 	self:addElement(listBox)
 	self.listBox = listBox
 
-	self:registerEventHandler("button_prompt_back", gameModeListBackEventHandler)
 	self:registerEventHandler("listbox_focus_changed", gameModeListFocusChangedEventHandler)
 	self:registerEventHandler("click", gameModeListSelectionClickedEventHandler)
 
@@ -244,18 +275,11 @@ function LUI.createMenu.SelectGameModeListZM(controller)
 end
 
 local function mapListFocusChangedEventHandler(self, event)
-	local focusedIndex = self.listBox:getFocussedIndex()
+	CoD.SelectMapListZombie.MapIndex = self.listBox:getFocussedIndex()
 end
 
 local function mapListSelectionClickedEventHandler(self, event)
-	local focusedIndex = self.listBox:getFocussedIndex()
-
-	if CoD.SelectMapListZombie.GameModeIndex ~= nil then
-		Engine.SetDvar("ui_gametype_index", CoD.SelectMapListZombie.GameModeIndex)
-		CoD.SelectMapListZombie.GameModeIndex = nil
-	end
-
-	Engine.SetDvar("ui_mapname_index", focusedIndex)
+	CoD.SelectMapListZombie.MapIndex = self.listBox:getFocussedIndex()
 
 	local prevTeamCount = Engine.GetGametypeSetting("teamCount")
 
@@ -284,8 +308,7 @@ local function mapListCreateButtonMutables(controller, mutables)
 end
 
 local function mapListGetButtonData(controller, index, mutables, self)
-	local map = CoD.SelectMapListZombie.Maps[index]
-	mutables.text:setText(Engine.Localize(map))
+	mutables.text:setText(Engine.Localize(CoD.SelectMapListZombie.Maps[index].localizedString))
 end
 
 function LUI.createMenu.SelectMapListZM(controller)
@@ -299,7 +322,11 @@ function LUI.createMenu.SelectMapListZM(controller)
 
 	self:addTitle(Engine.Localize("MPUI_MAPS_CAPS"))
 
-	local index = math.max(1, UIExpression.DvarInt(nil, "ui_mapname_index"))
+	local index = 1
+
+	if UIExpression.DvarString(nil, "ui_gametype") == "zclassic" then
+		index = getKeyValueIndex(CoD.SelectMapListZombie.Maps, "ui_mapname", UIExpression.DvarString(nil, "ui_mapname"))
+	end
 
 	local listBox = CoD.ListBox.new(nil, controller, 15, CoD.CoD9Button.Height, 250, mapListCreateButtonMutables, mapListGetButtonData, 5, 0)
 	listBox:setLeftRight(true, false, 0, 250)
@@ -319,18 +346,11 @@ function LUI.createMenu.SelectMapListZM(controller)
 end
 
 local function locationListFocusChangedEventHandler(self, event)
-	local focusedIndex = self.listBox:getFocussedIndex()
+	CoD.SelectMapListZombie.LocationIndex = self.listBox:getFocussedIndex()
 end
 
 local function locationListSelectionClickedEventHandler(self, event)
-	local focusedIndex = self.listBox:getFocussedIndex()
-
-	if CoD.SelectMapListZombie.GameModeIndex ~= nil then
-		Engine.SetDvar("ui_gametype_index", CoD.SelectMapListZombie.GameModeIndex)
-		CoD.SelectMapListZombie.GameModeIndex = nil
-	end
-
-	Engine.SetDvar("ui_zm_mapstartlocation_index", focusedIndex)
+	CoD.SelectMapListZombie.LocationIndex = self.listBox:getFocussedIndex()
 
 	local prevTeamCount = Engine.GetGametypeSetting("teamCount")
 
@@ -359,8 +379,7 @@ local function locationListCreateButtonMutables(controller, mutables)
 end
 
 local function locationListGetButtonData(controller, index, mutables, self)
-	local location = CoD.SelectMapListZombie.Locations[index]
-	mutables.text:setText(Engine.Localize(location))
+	mutables.text:setText(Engine.Localize(CoD.SelectMapListZombie.Locations[index].localizedString))
 end
 
 function LUI.createMenu.SelectLocationListZM(controller)
@@ -374,7 +393,11 @@ function LUI.createMenu.SelectLocationListZM(controller)
 
 	self:addTitle(Engine.Localize("MPUI_MAPS_CAPS"))
 
-	local index = math.max(1, UIExpression.DvarInt(nil, "ui_zm_mapstartlocation_index"))
+	local index = 1
+
+	if UIExpression.DvarString(nil, "ui_gametype") ~= "zclassic" then
+		index = getKeyValueIndex(CoD.SelectMapListZombie.Locations, "ui_zm_mapstartlocation", UIExpression.DvarString(nil, "ui_zm_mapstartlocation"))
+	end
 
 	local listBox = CoD.ListBox.new(nil, controller, 15, CoD.CoD9Button.Height, 250, locationListCreateButtonMutables, locationListGetButtonData, 5, 0)
 	listBox:setLeftRight(true, false, 0, 250)
